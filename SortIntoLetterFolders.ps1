a477a7c65b726ec9938753be507805c6f96bb996
UpdogUpdogUpdog <31091051+UpdogUpdogUpdog@users.noreply.github.com>
2025-04-13 11:53:59 -0400

    Add files via upload

---
+++ SortIntoLetterFolders.ps1
@@ -0,0 +1,17 @@
+﻿$list = @()
+65..90 | foreach {
+    $letter=[char]$_
+    New-Item -Path .\ -Name $letter -ItemType "directory"  
+    move-item .\$letter`?* $letter
+    $list +=  $letter
+}
+
+$rest=get-childitem -path .\ -exclude $list 
+New-Item -Path .\ -Name "#" -ItemType "directory"
+
+foreach ($f in $rest) {
+    move-item  -literalpath $f.fullname ".\#"
+}
+
+#Remove empty directories
+Get-ChildItem -Directory -Recurse | Where-Object { -Not (Get-ChildItem $_.FullName) } | Remove-Item -Force
